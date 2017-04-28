f2a4ddff128780b2835294fe25a5ea31bf045dc0
Kris_Chan <743628145@qq.com>
2017-04-28 20:22:44 +0800

    1.递归之奇技淫巧;

--- JavaScript/Demo9/static9.js
+++ JavaScript/Demo9/static9.js
@@ -1,11 +1,17 @@
 'use strict';
-function f(x){
-if(undefined === x){
-  return 0;
-}
-return x + function g(x){
-  return f(x-1);
-}
+//递归之奇技淫巧
+const f = (a) => {
+  if (a === undefined) {
+    return 0;
+  }
+  else {
+    return (b) => {
+      if (b === undefined) {
+        return a;
+      }
+      return f(a + b)
+    }
+  }
 }
 console.log(f());
-console.log( f(1)(2)(3)(4)(5)() );
+console.log( f(1)(2)(3)(4)(5)(6)() );
