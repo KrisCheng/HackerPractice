3aa44ff7114e5ce9e1b6bb19e410272696ecccf4
Kris_Chan <743628145@qq.com>
2017-05-07 14:28:46 +0800

    1.add a demo;

--- JavaScript/Basic/DOM/Demo1/static.js
+++ JavaScript/Basic/DOM/Demo1/static.js
@@ -1,2 +1,6 @@
-var height = 'about 5\'10\" tall';
-window.alert(height);
+'use strict';
+//获取节点的三个原始方法
+var tmp1 = document.getElementById("test");// 返回一个对象
+var tmp2 = document.getElementsByTagName("span");// 返回一个对象数组
+var tmp3 = document.getElementsByClassName("tests");// 返回一个对象数组
+console.log(tmp1);
