7516207cd5353fc3a9d5552134a2ed4665a2bb97
Kris_Chan <743628145@qq.com>
2017-05-22 23:03:54 +0800

    1.add 2 error demos;

---
+++ Python/error/assert.py
@@ -0,0 +1,13 @@
+# assert
+import logging
+logging.basicConfig(level=logging.INFO)
+def foo(s):
+    n = int(s)
+    logging.info('n = %d' % n)
+    # assert n != 0, 'n is zero!'
+    return 10 / n
+
+def main():
+    foo('0')
+
+main()
